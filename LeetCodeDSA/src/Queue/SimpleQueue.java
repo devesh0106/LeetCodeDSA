370a9bc280d1b9471fa3104de3425ec3f8bded7d
Devesh Anand <deveshlive2912@gmail.com>
2025-07-06 19:36:43 +0530

    Leetcode

---
+++ LeetCodeDSA/src/Queue/SimpleQueue.java
@@ -0,0 +1,74 @@
+package Queue;
+
+public class SimpleQueue {
+
+    private int[] queue;
+    private int maxSize;
+    private int front;
+    private int rear;
+
+    public SimpleQueue(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new int[maxSize];
+        this.front = 0; // pointer to remove elements
+        this.rear = -1; //pointer to add elements
+    }
+
+    //Adding Elements in Queue
+    public void enqueue(int data){
+        if(rear==maxSize-1){
+            System.out.println("Queue is Full");
+        }
+        else{
+            rear++;
+            queue[rear]=data;
+        }
+    }
+
+    //Deleting Elements from Queue
+    public int dequeue(){
+        if(front>rear){
+            System.out.println("Queue is Empty");
+        }
+        else {
+            int data = queue[front];
+            front++;
+            return data;
+        }
+        return -1;
+    }
+
+    //Peek
+    public int peek(){
+        if(front>rear){
+            System.out.println("Queue is Empty");
+        }
+        else {
+            return queue[front];
+        }
+        return -1;
+    }
+
+    //Display
+    public void display(){
+        if(front>rear){
+            System.out.println("Queue is Empty, cant display");
+        }
+        for(int i=front;i<=rear;i++){
+            System.out.println("Data  inserted is "+queue[i]+"\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleQueue simpleQueue = new SimpleQueue(5);
+        simpleQueue.enqueue(5);
+        simpleQueue.enqueue(6);
+        simpleQueue.enqueue(7);
+        simpleQueue.enqueue(8);
+        simpleQueue.enqueue(9);
+        simpleQueue.display();
+        System.out.println("Data at the front/peek is " + simpleQueue.peek());
+        System.out.println("Data is removed from front is  "+simpleQueue.dequeue()+"\n");
+        simpleQueue.display();
+    }
+}
