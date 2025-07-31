d975913e5dd1fdf116f16359f4cbda1d2c3ab959
Devesh Anand <deveshlive2912@gmail.com>
2025-07-31 07:47:19 +0530

    problems

--- LeetCodeDSA/src/Concepts/Random/ValidPalindrome.java
+++ LeetCodeDSA/src/Concepts/Random/ValidPalindrome.java
@@ -1,17 +1,5 @@
 package Concepts.Random;
-/*
-Given a string s, return true if it is a palindrome, otherwise return false.
 
-A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
-
-Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
-
-Example 1:
-
-Input: s = "Was it a car or a cat I saw?"
-
-Output: true
-* */
 public class ValidPalindrome {
     public boolean validPalindromes(String str){
         //Step 1 : to remove white spaces from string
